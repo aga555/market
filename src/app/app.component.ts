75c09f5520517b730c85ac21e9ef7c9fd6024c3a
akrawczyk <agammkrawczyk@gmail.com>
2020-04-26 20:01:53 +0200

    refactor  move ofers to  component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {Offer} from './Offer';
 
 
 @Component({
@@ -9,28 +8,4 @@ import {Offer} from './Offer';
 })
 export class AppComponent {
 
-  offers: Offer[] = [
-    {
-      name: 'manicure',
-      date: new Date('01/04/2020'),
-      price: 50,
-      description: 'perfect manicure for everyone',
-      img: 'https://img.freepik.com/darmowe-zdjecie/kobiet-reki-i-kwiaty-z-kopii-przestrzenia_23-2148226454.jpg?size=626&ext=jpg'
-    },
-
-    {
-      name: 'photograph',
-      date: new Date('02/04/2020'),
-      price: 250,
-      description: 'weeding photography,  business photo',
-      img: 'https://image.freepik.com/darmowe-zdjecie/widok-z-gory-aparatu-dslr-filizanka-herbaty-notes-spiralny-dlugopis-obiektyw-aparatu-i-torba-na-drewnianym-stole_23-2148038885.jpg'
-    },
-    {
-      name: 'makeup ',
-      date: new Date('02/04/2020'),
-      price: 250,
-      description: 'make up artist',
-      img: 'https://img.freepik.com/darmowe-zdjecie/lustro-z-cieniami-do-powiek-i-szczotkami-na-rozowym-stole_23-2148047056.jpg?size=626&ext=jpg'
-    }
-  ];
 }
