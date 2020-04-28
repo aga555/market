b1d5778471a40a4c0f61041afee92f7d64947978
akrawczyk <agammkrawczyk@gmail.com>
2020-04-28 15:49:43 +0200

    router service  add to constructor dpendency to router  and use in methods cancel and login (navigate to home)

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {LoginService} from '../../services/login.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ export class LoginComponent implements OnInit {
   username: string;
   password: string;
 
-  constructor(private  loginService: LoginService) {
+  constructor(private  loginService: LoginService, private  router: Router) {
   }
 
   ngOnInit(): void {
@@ -20,12 +21,14 @@ export class LoginComponent implements OnInit {
     this.loginService.logIn(this.username, this.password);
     this.username = '';
     this.password = '';
+    this.router.navigate(['/home']);
 
   }
 
   cancel() {
     this.username = '';
     this.password = '';
+    this.router.navigate(['/home']);
 
   }
 }
